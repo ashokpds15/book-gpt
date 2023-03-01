794c1898bfdc634e1d73f0f3c28e5b3a2704ad14
ashok <ashokpds15@gmail.com>
2023-03-01 10:47:52 +0545

    push to git

---
+++ README.md
@@ -0,0 +1,29 @@
+# book-gpt
+
+Upload a book, ask me anything about it.
+
+https://book-gpt.vercel.app
+
+## Development
+
+```
+npm install
+```
+
+Start dev
+
+```
+npm run dev
+```
+
+## Roadmap
+
+- [ ] Make site responsive
+- [ ] Support more format
+- [ ] Include metadata in answer
+- [ ] Have an idea? Create an issue.
+
+## Credit:
+
+* [ui](https://github.com/shadcn/ui): Beautifully designed components built with Radix UI and Tailwind CSS.
+* [langchainjs](https://hwchase17.github.io/langchainjs/docs/overview/)
---
+++ pages/api/ingest.ts
@@ -0,0 +1,83 @@
+import type { NextApiRequest, NextApiResponse, PageConfig } from "next"
+import { PineconeClient } from "@pinecone-database/pinecone"
+import { Document } from "langchain/document"
+import { OpenAIEmbeddings } from "langchain/embeddings"
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
+import { PineconeStore } from "langchain/vectorstores"
+
+import { fileConsumer, formidablePromise } from "@/lib/formidable"
+import { getTextContentFromPDF } from "@/lib/pdf"
+
+const PINECONE_INDEX_NAME = "book-gpt"
+
+const formidableConfig = {
+  keepExtensions: true,
+  maxFileSize: 10_000_000,
+  maxFieldsSize: 10_000_000,
+  maxFields: 7,
+  allowEmptyFiles: false,
+  multiples: false,
+}
+
+export async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const chunks: never[] = []
+
+  const { fields, files } = await formidablePromise(req, {
+    ...formidableConfig,
+    // consume this, otherwise formidable tries to save the file to disk
+    fileWriteStreamHandler: () => fileConsumer(chunks),
+  })
+
+  const fileData = Buffer.concat(chunks)
+  const openaiApiKey = fields["openai-api-key"]
+  const pineconeApiKey = fields["pinecone-api-key"]
+
+  const { file } = files
+  let fileText = ""
+
+  switch (file.mimetype) {
+    case "text/plain":
+      fileText = fileData.toString()
+      break
+    case "application/pdf":
+      fileText = await getTextContentFromPDF(fileData)
+      break
+    case "application/octet-stream":
+      fileText = fileData.toString()
+      break
+    default:
+      throw new Error("Unsupported file type.")
+  }
+
+  const rawDocs = new Document({ pageContent: fileText })
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+  })
+  const docs = textSplitter.splitDocuments([rawDocs])
+
+  const pinecone = new PineconeClient()
+  await pinecone.init({
+    environment: "us-west1-gcp",
+    apiKey: pineconeApiKey,
+  })
+
+  const index = pinecone.Index(PINECONE_INDEX_NAME)
+  await PineconeStore.fromDocuments(
+    index,
+    docs,
+    new OpenAIEmbeddings({
+      openAIApiKey: openaiApiKey,
+    })
+  )
+
+  res.status(200).json({})
+}
+
+export const config: PageConfig = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+export default handler
